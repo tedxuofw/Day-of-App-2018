08aca58d52f1e12a3d30ea91fde0b47ea21d1166
Jenny Liang <jennyliang@D-10-18-240-227.dhcp4.washington.edu>
2018-04-29 00:48:26 -0700

    Created schedule file

---
+++ public/resources/data/schedule.js
@@ -0,0 +1,138 @@
+export default schedule = [
+    {
+        "name": "John Sinclair",
+        "start": "May 5, 2018 10:10:00",
+        "end": "May 5, 2018 10:23:00",
+        "talk": "", 
+        "shift": "-140px", 
+        "img": "/resources/john.png"
+    },
+    {
+        "name": "Kristi Straus",
+        "start": "May 5, 2018 10:23:00",
+        "end": "May 5, 2018 10:36:00",
+        "talk": "",
+        "shift": "-95px", 
+        "img": "/resources/kristi.png"
+    },
+    {
+        "name": "Fauzia Lala",
+        "start": "May 5, 2018 10:36:00",
+        "end": "May 5, 2018 10:49:00", 
+        "talk": "", 
+        "shift": "-150px", 
+        "img": "/resources/fauzia.png"
+    },
+    {
+        "name": "Aura",
+        "start": "May 5, 2018 10:49:00",
+        "end": "May 5, 2018 11:01:00",
+        "talk": "",
+        "shift": "",
+        "img": ""
+    },
+    {
+        "name": "Izdihar Bailey",
+        "start": "May 5, 2018 11:01:00",
+        "end": "May 5, 2018 11:14:00",
+        "talk": "", 
+        "shift": "-130px",
+        "img": "/resources/izdihar.png"
+    }, 
+    {
+        "name": "Jeanne Suchodolski",
+        "start": "May 5, 2018 11:14:00",
+        "end": "May 5, 2018 11:27:00", 
+        "talk": "",
+        "shift": "-70px", 
+        "img": "/resources/jeanne.png"
+    },
+    {
+        "name": "Heather Evans",
+        "start": "May 5, 2018 12:27:00",
+        "end": "May 5, 2018 12:40:00",
+        "talk": "",
+        "shift": "",
+        "img": ""
+    },
+    {
+        "name": "Human Condition",
+        "start": "May 5, 2018 12:40:00",
+        "end": "May 5, 2018 12:52:00",
+        "talk": "",
+        "shift": "",
+        "img": ""
+    },
+    {
+        "name": "Ansel Santosa",
+        "start": "May 5, 2018 12:52:00",
+        "end": "May 5, 2018 13:05:00",
+        "talk": "", 
+        "shift": "-120px", 
+        "img": "/resources/ansel.png"
+    },
+   {
+        "name": "Jeffrey Lew",
+        "start": "May 5, 2018 13:05:00",
+        "end": "May 5, 2018 13:18:00",
+        "talk": "", 
+        "shift": "-150px", 
+        "img": "/resources/jeffery.png"
+    },
+    {
+        "name": "Tyler Valentine",
+        "start": "May 5, 2018 13:28:00",
+        "end": "May 5, 2018 13:41:00",
+        "talk": "", 
+        "shift": "-170px", 
+        "img": "/resources/tyler.png"
+    },
+    {
+        "name": "UW Awaaz",
+        "start": "May 5, 2018 14:26:00",
+        "end": "May 5, 2018 14:38:00",
+        "talk": "",
+        "shift": "",
+        "img": ""
+    },
+    {
+        "name": "Dr. Steve Bezruchka",
+        "start": "May 5, 2018 14:38:00",
+        "end": "May 5, 2018 14:51:00",
+        "talk": "", 
+        "shift":"-120px", 
+        "img": "/resources/bezruchka.jpg"
+    },
+    {
+        "name": "Tarah Wheeler",
+        "start": "May 5, 2018 14:51:00",
+        "end": "May 5, 2018 15:04:00",
+        "talk": "", 
+        "shift": "-130px", 
+        "img": "/resources/tarah.png"
+    },
+    {
+        "name": "Karen Liftin",
+        "start": "May 5, 2018 15:14:00",
+        "end": "May 5, 2018 15:27:00",
+        "talk":"", 
+        "shift": "-100px", 
+        "img": "/resources/karen.png"
+    },
+    {
+        "name": "Andrea Weatherhead",
+        "start": "May 5, 2018 15:27:00",
+        "end": "May 5, 2018 15:40:00",
+        "talk": "", 
+        "shift": "-140px", 
+        "img": "/resources/andrea.png"
+    },
+    {
+        "name": "Sarah Myhre",
+        "start": "May 5, 2018 15:40:00",
+        "end": "May 5, 2018 15:53:00",
+        "talk": "", 
+        "shift": "-100px", 
+        "img": "/resources/sarah.png"
+    }
+];
